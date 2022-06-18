a0a132a749e3edc3c598b68c8b3f0e497b0cc9af
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-06-18 13:12:01 +0200

    add script for switching with sudo to other users

---
+++ sudo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PAC=`id -nu`
+
+if ! [ -d ~/users ]; then
+    echo "kein ~/users Verzeichnis vorhanden - Abbruch"
+    exit 0
+fi
+
+if [ -z $1 ]; then
+    echo "Aufruf: ~/bin/sudo.sh mustermann"
+    echo "   um in den Benutzer $PAC-mustermann zu wechseln"
+    exit 0
+fi
+
+username=$1
+
+if [ ! -d $HOME/users/$username ]
+then
+    echo "Kann den Benutzer $username nicht finden"
+
+    if [ ! -z "`ls $HOME/users -1 | grep $username`" ]
+    then
+      echo
+      echo "Es gibt diese Benutzer, die ähnlich sind:"
+      ls $HOME/users -1 | grep $username
+    fi
+
+    exit -1
+fi
+
+# $HOME/.profile should have: cd ~
+sudo -u `whoami`"-$username" bash --login
+
