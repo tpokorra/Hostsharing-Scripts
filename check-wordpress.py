200c69dc8ee6b7c90be66fa55b4b6671e890b959
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2023-01-03 09:38:58 +0100

    check-wordpress: also look in current pac domains directly. format output of wordpress installations

--- check-wordpress.py
+++ check-wordpress.py
@@ -2,64 +2,52 @@
 
 import os
 import getpass
+import subprocess
 
 description = """
 Check Wordpress installations of my Hostsharing users
 """
 
-def get_installation(pac, user, path, version_file):
-    cmd = f'if [ -f "{path}/{version_file}" ]; then echo "{path}"; fi'
-    cmd = cmd.replace('"', '\\"')
-    stream = os.popen(f'sudo -u {pac}-{user} -s /bin/bash -c "{cmd}"')
-    path = stream.read()
-    return path.strip()
+def get_subdomains_of_domain(user, domain):
 
-
-def get_installations(pac, user):
-    version_file = "wp-includes/version.php"
-    path = f"/home/pacs/{pac}/users/{user}/doms"
-    installations = []
-    domains = get_domains(pac, user)
-    for domain in (domains or []):
-        installation = get_installation(pac, user, f"{path}/{domain}/htdocs-ssl", version_file)
-        if installation:
-            installations.append((domain, installation))
-
-        subdomains = get_subdomains(pac, user, domain)
-        for subdomain in (subdomains or []):
-            installation = get_installation(pac, user, f"{path}/{domain}/subs-ssl/{subdomain}", version_file)
-            if installation:
-                installations.append((f"{subdomain}.{domain}", installation))
-
-    for (domain, instance_path) in installations:
-        cmd = f'cat "{instance_path}/{version_file}" | grep "wp_version = "'
-        stream = os.popen(f'sudo -u {pac}-{user} {cmd}')
-        version = stream.read().strip()
-        if version:
-            print(f'{pac}-{user}: {domain}: {version} {instance_path}')
-
-
-def get_subdomains(pac, user, domain):
-    path = f"/home/pacs/{pac}/users/{user}/doms/{domain}/subs-ssl"
-    stream = os.popen(f'sudo -u {pac}-{user} -s /bin/bash -c "if [ -d {path} ]; then ls -1 {path}; fi"')
-    domains = stream.read()
-    if domains:
-        return domains.split('\n')
-    return None
-
-def get_domains(pac, user):
-    path = f"/home/pacs/{pac}/users/{user}/doms"
-    stream = os.popen(f'sudo -u {pac}-{user} -s /bin/bash -c "if [ -d {path} ]; then ls -1 {path}; fi"')
-    domains = stream.read()
-    if domains:
-        return domains.split('\n')
+    path = f"~/doms/{domain}/subs-ssl"
+    stream = os.popen(f'sudo -u {user} -s /bin/bash -c "if [ -d {path} ]; then ls -1 {path}; fi"')
+    subdomains = stream.read()
+    if subdomains:
+        return subdomains.split('\n')
     return None
 
 
-def get_users(pac):
+def get_domains_of_user(user):
+
+    result = {}
+    cmd = "if [ -d ~/doms ]; then ls ~/doms -1; fi"
+    output = subprocess.getoutput(f'sudo -u {user} -s /bin/bash -c "{cmd}"').split('\n')
+    for line in output:
+        if line:
+            result[line] = {'domain': line, 'user': user}
+    return result
+
+
+def get_domains_of_pac(pac):
+
+    result = {}
+
+    result = get_domains_of_user(pac)
+    users = get_users_of_pac(pac)
+    for user in (users or []):
+        result.update(get_domains_of_user(f"{pac}-{user}"))
+
+    return result
+
+def get_current_pac():
+    user = getpass.getuser()
+    return user.split('-')[0]
+
+
+def get_users_of_pac(pac):
     if not os.path.isdir(f'/home/pacs/{pac}/users'):
-        print("Bitte als Paket Admin (eg xyz00) aufrufen")
-        exit(-1)
+        return None
 
     result = []
     for f in os.scandir(f'/home/pacs/{pac}/users'):
@@ -68,11 +56,51 @@ def get_users(pac):
 
     return result
 
+def get_wordpress_installation_in_path(user, domain, path):
+    version_file = "wp-includes/version.php"
+    cmd = f'if [ -f {path}/{version_file} ]; then cat {path}/{version_file} | grep "wp_version = "; fi'
+    cmd = cmd.replace('"', '\\"')
+    stream = os.popen(f'sudo -u {user} -s /bin/bash -c "{cmd}"')
+    version = stream.read().strip()
+    if version:
+        return {'user': user, 'domain': domain, 'path': path, 'version': version.split('=')[1].strip(" ;'")}
+    return None
 
-# check all users
-pac=getpass.getuser()
-users = get_users(pac)
 
-for user in users:
-    domains = get_installations(pac, user)
+def get_wordpress_installations_of_user(user):
+
+    installations = []
+    domains = get_domains_of_user(user)
+
+    for domain in (domains or []):
+        installation = get_wordpress_installation_in_path(user, domain, f'~/doms/{domain}/htdocs-ssl')
+        if installation:
+            installations.append(installation)
+
+        subdomains = get_subdomains_of_domain(user, domain)
+        for subdomain in (subdomains or []):
+            installation = get_wordpress_installation_in_path(user, f'{subdomain}.{domain}', f'~/doms/{domain}/subs-ssl/{subdomain}')
+            if installation:
+                installations.append(installation)
+
+    return installations
+
+
+def get_wordpress_installations_of_pac(pac):
+
+    result = []
+    installations = get_wordpress_installations_of_user(pac)
+    if installations:
+        result += installations
+    users = get_users_of_pac(pac)
+    for user in (users or []):
+        installations = get_wordpress_installations_of_user(f"{pac}-{user}")
+        if installations:
+            result += installations
+    return result
+
+pac = get_current_pac()
+installations = get_wordpress_installations_of_pac(pac)
+for i in installations:
+    print(f"{i['user']:<20} {i['domain']:<40} {i['version']:10} {i['path']}")
 
