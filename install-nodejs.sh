3469c96030c01309085f363c029c9f6280c2235c
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2024-01-05 14:18:55 +0100

    Update install-nodejs.sh
    add links to nodejs releases
    use nodejs 20

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -3,14 +3,14 @@
 mkdir -p ~/bin
 cd ~/bin
 
-# see https://nodejs.org/de/download
-version=18
+# see https://nodejs.org and https://nodejs.org/en/about/previous-releases
+version=20
 
 touch ~/.profile
 chmod u+x ~/.profile
 cd /tmp
 # see https://github.com/nvm-sh/nvm/releases
-wget https://raw.githubusercontent.com/creationix/nvm/v0.39.5/install.sh
+wget https://raw.githubusercontent.com/creationix/nvm/v0.39.7/install.sh
 chmod u+x install.sh
 ./install.sh
 rm ./install.sh
