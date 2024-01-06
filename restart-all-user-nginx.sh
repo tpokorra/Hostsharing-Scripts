5ed89e7333f7320b471f4e53f9daf13ce6013b62
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2024-01-06 09:59:57 +0100

    only try to restart nginx if service file exists

--- restart-all-user-nginx.sh
+++ restart-all-user-nginx.sh
@@ -2,4 +2,4 @@
 
 PAC=`id -nu`
 cd ~/users
-for u in *; do sudo -u $PAC-$u /bin/bash -c "whoami && source ~/.profile && systemctl --user restart nginx"; done
+for u in *; do sudo -u $PAC-$u /bin/bash -c "if [ -f ~/.config/systemd/user/nginx.service ]; then whoami && source ~/.profile && systemctl --user restart nginx; fi"; done
